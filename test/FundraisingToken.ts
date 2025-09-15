deae1c5d5342700195e2e7ed474814e456395616
X-spec7 <mercury.spec77@gmail.com>
2025-09-16 04:50:14 +0900

    feat: update fundraising token testing

--- test/FundraisingToken.ts
+++ test/FundraisingToken.ts
@@ -1,10 +1,9 @@
 import { expect } from "chai";
-import { network } from "hardhat";
-
-const { ethers } = await network.connect();
+import { ethers } from "hardhat";
+import { FundraisingToken } from "../typechain-types";
 
 describe("FundraisingToken", function () {
-  let token: any;
+  let token: FundraisingToken;
   let owner: any;
   let user1: any;
   let user2: any;
@@ -12,13 +11,13 @@ describe("FundraisingToken", function () {
 
   beforeEach(async function () {
     [owner, user1, user2, user3] = await ethers.getSigners();
-    token = await ethers.deployContract("FundraisingToken", ["FundraisingToken", "FRT", 18, 1000000]);
+    token = await ethers.deployContract("FundraisingToken", ["FundraisingToken", "FRT", 18, ethers.parseEther("1000000")]);
   });
 
   it("Should have correct initial supply", async function () {
     const totalSupply = await token.totalSupply();
     const ownerBalance = await token.balanceOf(owner.address);
-    
+
     expect(totalSupply).to.equal(ethers.parseUnits("1000000", 18));
     expect(ownerBalance).to.equal(ethers.parseUnits("1000000", 18));
   });
@@ -37,9 +36,11 @@ describe("FundraisingToken", function () {
     expect(await token.isMintAuthorityFrozen()).to.be.true;
   });
 
+  it("Should have reward tracking disabled initially", async function () {
+    expect(await token.isRewardTrackingEnabled()).to.be.false;
+  });
+
   it("Should add address to whitelist", async function () {
-    expect(await token.isWhitelisted(user1.address)).to.be.false;
-    
     await token.addToWhitelist(user1.address);
     expect(await token.isWhitelisted(user1.address)).to.be.true;
   });
@@ -53,145 +54,91 @@ describe("FundraisingToken", function () {
   });
 
   it("Should batch add addresses to whitelist", async function () {
-    const addresses = [user1.address, user2.address];
-    
+    const addresses = [user1.address, user2.address, user3.address];
     await token.batchAddToWhitelist(addresses);
     
     expect(await token.isWhitelisted(user1.address)).to.be.true;
     expect(await token.isWhitelisted(user2.address)).to.be.true;
+    expect(await token.isWhitelisted(user3.address)).to.be.true;
   });
 
   it("Should batch remove addresses from whitelist", async function () {
-    await token.addToWhitelist(user1.address);
-    await token.addToWhitelist(user2.address);
+    const addresses = [user1.address, user2.address, user3.address];
+    await token.batchAddToWhitelist(addresses);
     
-    const addresses = [user1.address, user2.address];
     await token.batchRemoveFromWhitelist(addresses);
     
     expect(await token.isWhitelisted(user1.address)).to.be.false;
     expect(await token.isWhitelisted(user2.address)).to.be.false;
+    expect(await token.isWhitelisted(user3.address)).to.be.false;
   });
 
   it("Should require both sender and recipient whitelisted for transfer", async function () {
-    const amount = ethers.parseUnits("1000", 18);
-    
-    // Should fail - user1 not whitelisted
-    await expect(
-      token.connect(user1).transfer(user2.address, amount)
-    ).to.be.revertedWithCustomError(token, "SenderNotWhitelisted");
-    
-    // Add user1 to whitelist
     await token.addToWhitelist(user1.address);
-    
-    // Should still fail - user2 not whitelisted
-    await expect(
-      token.connect(user1).transfer(user2.address, amount)
-    ).to.be.revertedWithCustomError(token, "RecipientNotWhitelisted");
-    
-    // Add user2 to whitelist
     await token.addToWhitelist(user2.address);
     
-    // Transfer some tokens to user1 first
-    await token.transfer(user1.address, amount);
+    const transferAmount = ethers.parseEther("100");
     
-    // Now transfer should work
-    await token.connect(user1).transfer(user2.address, amount);
+    // Should succeed when both are whitelisted
+    await token.transfer(user2.address, transferAmount);
+    expect(await token.balanceOf(user2.address)).to.equal(transferAmount);
     
-    expect(await token.balanceOf(user2.address)).to.equal(amount);
+    // Should fail when recipient is not whitelisted
+    await expect(
+      token.transfer(user3.address, transferAmount)
+    ).to.be.revertedWithCustomError(token, "RecipientNotWhitelisted");
   });
 
   it("Should require both sender and recipient whitelisted for transferFrom", async function () {
-    const amount = ethers.parseUnits("1000", 18);
-    
-    // Add users to whitelist (but NOT user3)
     await token.addToWhitelist(user1.address);
     await token.addToWhitelist(user2.address);
-    // user3 is NOT whitelisted
     
-    // Transfer tokens to user1
-    await token.transfer(user1.address, amount);
+    const transferAmount = ethers.parseEther("100");
+    await token.approve(user1.address, transferAmount);
     
-    // Approve user2 to spend user1's tokens
-    await token.connect(user1).approve(user2.address, amount);
+    // Should succeed when both are whitelisted
+    await token.connect(user1).transferFrom(owner.address, user2.address, transferAmount);
+    expect(await token.balanceOf(user2.address)).to.equal(transferAmount);
     
-    // user3 tries to transferFrom (not whitelisted) - this should fail with RecipientNotWhitelisted
+    // Should fail when recipient is not whitelisted
     await expect(
-      token.connect(user3).transferFrom(user1.address, user3.address, amount)
+      token.connect(user1).transferFrom(owner.address, user3.address, transferAmount)
     ).to.be.revertedWithCustomError(token, "RecipientNotWhitelisted");
-    
-    // Add user3 to whitelist
-    await token.addToWhitelist(user3.address);
-    
-    // user2 transfers (whitelisted)
-    await token.connect(user2).transferFrom(user1.address, user3.address, amount);
-    
-    expect(await token.balanceOf(user3.address)).to.equal(amount);
   });
 
   it("Should require whitelist for approve", async function () {
-    const amount = ethers.parseUnits("1000", 18);
-    
-    // user1 not whitelisted
     await expect(
-      token.connect(user1).approve(user2.address, amount)
+      token.connect(user1).approve(user2.address, ethers.parseEther("100"))
     ).to.be.revertedWithCustomError(token, "CallerNotWhitelisted");
-    
-    // Add user1 to whitelist
-    await token.addToWhitelist(user1.address);
-    
-    // user2 not whitelisted
-    await expect(
-      token.connect(user1).approve(user2.address, amount)
-    ).to.be.revertedWithCustomError(token, "SpenderNotWhitelisted");
-    
-    // Add user2 to whitelist
-    await token.addToWhitelist(user2.address);
-    
-    // Now approve should work
-    await token.connect(user1).approve(user2.address, amount);
-    
-    expect(await token.allowance(user1.address, user2.address)).to.equal(amount);
   });
 
   it("Should fail to mint when authority is frozen", async function () {
-    const mintAmount = ethers.parseUnits("50000", 18);
-    
-    // Add user1 to whitelist
-    await token.addToWhitelist(user1.address);
-    
-    // Should fail - mint authority is frozen
     await expect(
-      token.mint(user1.address, mintAmount)
+      token.mint(user1.address, ethers.parseEther("1000"))
     ).to.be.revertedWithCustomError(token, "MintAuthorityFrozen");
   });
 
   it("Should burn tokens correctly", async function () {
-    const burnAmount = ethers.parseUnits("10000", 18);
-    
-    // Should fail - user1 not whitelisted
-    await expect(
-      token.connect(user1).burn(burnAmount)
-    ).to.be.revertedWithCustomError(token, "CallerNotWhitelisted");
-    
-    // Add user1 to whitelist and give tokens
     await token.addToWhitelist(user1.address);
-    await token.transfer(user1.address, burnAmount);
+    const burnAmount = ethers.parseEther("100");
     
-    // Now burn should work
     await token.connect(user1).burn(burnAmount);
-    
-    expect(await token.balanceOf(user1.address)).to.equal(0);
-    expect(await token.totalSupply()).to.equal(ethers.parseUnits("990000", 18));
+    expect(await token.balanceOf(user1.address)).to.equal(ethers.parseEther("999900"));
   });
 
   it("Should only allow owner to manage whitelist", async function () {
     await expect(
       token.connect(user1).addToWhitelist(user2.address)
     ).to.be.revertedWithCustomError(token, "CallerNotOwner");
+  });
+
+  it("Should set reward tracking address and enable it", async function () {
+    const mockRewardTracking = ethers.Wallet.createRandom().address;
     
-    await expect(
-      token.connect(user1).removeFromWhitelist(user2.address)
-    ).to.be.revertedWithCustomError(token, "CallerNotOwner");
+    await token.setRewardTrackingAddress(mockRewardTracking);
+    
+    expect(await token.rewardTrackingAddress()).to.equal(mockRewardTracking);
+    expect(await token.isRewardTrackingEnabled()).to.be.true;
   });
 
   it("Should emit whitelist events", async function () {
@@ -205,25 +152,18 @@ describe("FundraisingToken", function () {
   });
 
   it("Should emit Transfer events", async function () {
-    const amount = ethers.parseUnits("1000", 18);
-    
     await token.addToWhitelist(user1.address);
-    await token.addToWhitelist(user2.address);
-    await token.transfer(user1.address, amount);
     
-    await expect(token.connect(user1).transfer(user2.address, amount))
+    await expect(token.transfer(user1.address, ethers.parseEther("100")))
       .to.emit(token, "Transfer")
-      .withArgs(user1.address, user2.address, amount);
+      .withArgs(owner.address, user1.address, ethers.parseEther("100"));
   });
 
   it("Should emit Approval events", async function () {
-    const amount = ethers.parseUnits("1000", 18);
-    
     await token.addToWhitelist(user1.address);
-    await token.addToWhitelist(user2.address);
     
-    await expect(token.connect(user1).approve(user2.address, amount))
+    await expect(token.approve(user1.address, ethers.parseEther("100")))
       .to.emit(token, "Approval")
-      .withArgs(user1.address, user2.address, amount);
+      .withArgs(owner.address, user1.address, ethers.parseEther("100"));
   });
 });
